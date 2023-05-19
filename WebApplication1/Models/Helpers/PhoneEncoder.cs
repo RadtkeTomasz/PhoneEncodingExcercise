cdf44cd99d3c541103532c10d15ed2201f554e26
Tomasz Radtke <tomasz.radtke@oponeo.pl>
2023-05-19 11:17:06 +0200

    mini fix 2

--- WebApplication1/Models/Helpers/PhoneEncoder.cs
+++ WebApplication1/Models/Helpers/PhoneEncoder.cs
@@ -6,34 +6,34 @@ namespace WebApplication1.Models.Helpers
 {
     public static class PhoneEncoder
     {
-        private static readonly Dictionary<string, string> _phoneKeyboardMappings = new Dictionary<string, string>()
+        private static readonly Dictionary<char, string> _phoneKeyboardMappings = new Dictionary<char, string>()
         {
-            { "a", "2"},
-            { "b", "22"},
-            { "c", "222" },
-            { "d", "3" },
-            { "e", "33" },
-            { "f", "333" },
-            { "g","4" },
-            { "h", "44" },
-            { "i", "444" },
-            { "j", "5" },
-            { "k", "55" },
-            { "l", "555" },
-            { "m", "6"},
-            { "n", "66" },
-            { "o", "666" },
-            { "p", "7" },
-            { "q", "77" },
-            { "r", "777" },
-            { "s", "7777" },
-            { "t", "8" },
-            { "u", "88" },
-            { "v", "888" },
-            { "w", "9" },
-            { "x", "99" },
-            { "y", "999" },
-            { "z", "9999" },
+            { 'a', "2"},
+            { 'b', "22"},
+            { 'c', "222" },
+            { 'd', "3" },
+            { 'e', "33" },
+            { 'f', "333" },
+            { 'g',"4" },
+            { 'h', "44" },
+            { 'i', "444" },
+            { 'j', "5" },
+            { 'k', "55" },
+            { 'l', "555" },
+            { 'm', "6"},
+            { 'n', "66" },
+            { 'o', "666" },
+            { 'p', "7" },
+            { 'q', "77" },
+            { 'r', "777" },
+            { 's', "7777" },
+            { 't', "8" },
+            { 'u', "88" },
+            { 'v', "888" },
+            { 'w', "9" },
+            { 'x', "99" },
+            { 'y', "999" },
+            { 'z', "9999" },
         };
         public static InputString EncodeStringToPhoneInput(InputString input)
         {
@@ -42,7 +42,7 @@ namespace WebApplication1.Models.Helpers
 
             foreach (var letter in inputLetters)
             {
-                if (_phoneKeyboardMappings.TryGetValue(letter.ToString(), out string value))
+                if (_phoneKeyboardMappings.TryGetValue(letter, out string value))
                 {
                     sb.Append('[').Append(value).Append(']');
                 }
