2fa9e8d1378df122dc2b929f8631b8fd265f82e7
Tomasz Radtke <tomasz.radtke@oponeo.pl>
2023-05-19 10:54:59 +0200

    Wyciagnieto z kontrolera logike do klasy phoneEncoder

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -2,41 +2,14 @@
 using System.Diagnostics;
 using System.Text;
 using WebApplication1.Models;
+using WebApplication1.Models.Helpers;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly Dictionary<string, string> _phoneKeyboardMappings = new Dictionary<string, string>() 
-        {
-            { "a", "2"},
-            { "b", "22"},
-            { "c", "222" },
-            { "d", "3" },
-            { "e", "33" },
-            { "f", "333" },
-            { "g","4" },
-            { "h", "44" },
-            { "i", "444" },
-            { "j", "5" },
-            { "k", "55" },
-            { "l", "555" },
-            { "m", "6"},
-            { "n", "66" },
-            { "o", "666" },
-            { "p", "7" },
-            { "q", "77" },
-            { "r", "777" },
-            { "s", "7777" },
-            { "t", "8" },
-            { "u", "88" },
-            { "v", "888" },
-            { "w", "9" },
-            { "x", "99" },
-            { "y", "999" },
-            { "z", "9999" },
-        };
+       
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -52,19 +25,7 @@ namespace WebApplication1.Controllers
         {
             if (ModelState.IsValid)
             {
-                var sb = new StringBuilder();
-                var inputLetters = input.Text.ToLower().ToArray();
-
-                foreach (var letter in inputLetters)
-                {
-                    if (_phoneKeyboardMappings.TryGetValue(letter.ToString(), out string value))
-                    {
-                        sb.Append('[').Append(value).Append(']');
-                    }
-                }
-                var result = sb.ToString();
-                var encodedString = new InputString();
-                encodedString.Text = result;
+                var encodedString = PhoneEncoder.EncodeStringToPhoneInput(input);
                 return View("Encoded", encodedString);
             }
             else
---
+++ WebApplication1/Models/Helpers/PhoneEncoder.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Text;
+using WebApplication1.Models;
+
+namespace WebApplication1.Models.Helpers
+{
+    public static class PhoneEncoder
+    {
+        private static readonly Dictionary<string, string> _phoneKeyboardMappings = new Dictionary<string, string>()
+        {
+            { "a", "2"},
+            { "b", "22"},
+            { "c", "222" },
+            { "d", "3" },
+            { "e", "33" },
+            { "f", "333" },
+            { "g","4" },
+            { "h", "44" },
+            { "i", "444" },
+            { "j", "5" },
+            { "k", "55" },
+            { "l", "555" },
+            { "m", "6"},
+            { "n", "66" },
+            { "o", "666" },
+            { "p", "7" },
+            { "q", "77" },
+            { "r", "777" },
+            { "s", "7777" },
+            { "t", "8" },
+            { "u", "88" },
+            { "v", "888" },
+            { "w", "9" },
+            { "x", "99" },
+            { "y", "999" },
+            { "z", "9999" },
+        };
+        public static InputString EncodeStringToPhoneInput(InputString input)
+        {
+            var sb = new StringBuilder();
+            var inputLetters = input.Text.ToLower().ToArray();
+
+            foreach (var letter in inputLetters)
+            {
+                if (_phoneKeyboardMappings.TryGetValue(letter.ToString(), out string value))
+                {
+                    sb.Append('[').Append(value).Append(']');
+                }
+            }
+            var result = sb.ToString();
+            var encodedString = new InputString();
+            encodedString.Text = result;
+            return encodedString;
+        }
+       
+    }
+}
